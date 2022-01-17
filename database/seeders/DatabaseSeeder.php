60aa44f95c4d276ce81f201f2d1f37c8aba84e7f
funsource <50074063+funsource@users.noreply.github.com>
2022-01-17 14:33:16 +0700

    Initialize

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +17,66 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Alvin maulana',
+            'email' => 'alvin@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Danang sudrajat',
+            'email' => 'Danang@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'SQL Server',
+            'slug' => 'sql-server'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+            'body' => 'Voluptatem vero assumenda magnam deleniti, quia ut dolor saepe, 
+            tempora ex eligendi sequi culpa, ipsum a perferendis mollitia excepturi. 
+            repellendus veniam! Recusandae, dignissimos incidunt sint a, temporibus cumque, nisi voluptatum laboriosam 
+            libero voluptas ipsam quibusdam perferendis modi magni adipisci at iusto! Hic alias dignissimos doloribus 
+            delectus reprehenderit voluptas saepe cumque? Veniam sunt libero ut sapiente animi aliquam a, qui earum est magnam, ',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+            'body' => 'Voluptatem vero assumenda magnam deleniti, quia ut dolor saepe, 
+            tempora ex eligendi sequi culpa, ipsum a perferendis mollitia excepturi. 
+            repellendus veniam! Recusandae, dignissimos incidunt sint a, temporibus cumque, nisi voluptatum laboriosam 
+            libero voluptas ipsam quibusdam perferendis modi magni adipisci at iusto! Hic alias dignissimos doloribus 
+            delectus reprehenderit voluptas saepe cumque? Veniam sunt libero ut sapiente animi aliquam a, qui earum est magnam, ',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+        
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+            'body' => 'Voluptatem vero assumenda magnam deleniti, quia ut dolor saepe, 
+            tempora ex eligendi sequi culpa, ipsum a perferendis mollitia excepturi. 
+            repellendus veniam! Recusandae, dignissimos incidunt sint a, temporibus cumque, nisi voluptatum laboriosam 
+            libero voluptas ipsam quibusdam perferendis modi magni adipisci at iusto! Hic alias dignissimos doloribus 
+            delectus reprehenderit voluptas saepe cumque? Veniam sunt libero ut sapiente animi aliquam a, qui earum est magnam, ',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,7 +4,7 @@
     @foreach ($posts as $item)
         <article class="container border-bottom mb-4">
             <h1><a href="/post/{{ $item -> slug }}" style="text-decoration:none">{{ $item -> title }}</a>  </h1>
-            <p>By Alvin Maulana on Programming</p>
+            <p>By {{ $item -> user -> name }} on {{ $item -> category -> name }}</p>
 
             {{ $item -> body }}
 
